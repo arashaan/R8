e1de3904888d6de5aabc03bad09ab6fb8ef58201
Arash Shabbeh <arashaan@live.com>
2020-10-20 01:09:45 +0330

    Add .GetTwoLetterCulture()

---
+++ R8.Lib/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace R8.Lib
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Returns base url in Development
+        /// </summary>
+        /// <param name="culture"></param>
+        public static string GetTwoLetterCulture(this CultureInfo culture)
+        {
+            var result = culture.TwoLetterISOLanguageName;
+            return result;
+        }
+    }
+}
