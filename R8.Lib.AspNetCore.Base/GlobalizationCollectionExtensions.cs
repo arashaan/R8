1694e3a4c0b50004a740d8bb979b49a0a287cc2c
Arash Shabbeh <arashaan@live.com>
2020-10-22 15:54:44 +0330

    Add GlobalizationCollection.GetHtmlString()

---
+++ R8.Lib.AspNetCore.Base/GlobalizationCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+
+using System.Globalization;
+using System.Web;
+
+namespace R8.Lib.AspNetCore.Base
+{
+    public static class GlobalizationCollectionExtensions
+    {
+        public static HtmlString GetHtmlString(this GlobalizationCollectionJson collection, CultureInfo culture = null)
+        {
+            var text = collection.GetLocale(culture ?? CultureInfo.CurrentCulture, false);
+            var html = new HtmlString(HttpUtility.HtmlDecode(text));
+            return html;
+        }
+    }
+}
