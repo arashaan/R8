577469a53dab5ef8020fb9fd7e3eb3b5d4ac87df
Arash Shabbeh <arashaan@live.com>
2020-11-09 21:10:51 +0330

    Add documentations.

--- src/netcoreapp3.1/R8.Lib/Paginator/PaginationPageModel.cs
+++ src/netcoreapp3.1/R8.Lib/Paginator/PaginationPageModel.cs
@@ -1,9 +1,23 @@
 ﻿namespace R8.Lib.Paginator
 {
+    /// <summary>
+    /// An object to passing data from component to view.
+    /// </summary>
     public class PaginationPageModel
     {
+        /// <summary>
+        /// Number of current page.
+        /// </summary>
         public int Num { get; set; }
+
+        /// <summary>
+        /// Is this number is current page ?!
+        /// </summary>
         public bool IsCurrent { get; set; }
+
+        /// <summary>
+        /// Do you have link for this page ?
+        /// </summary>
         public string Link { get; set; }
     }
 }
