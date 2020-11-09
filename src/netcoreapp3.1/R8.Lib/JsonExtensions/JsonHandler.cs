b5eb53d3a7f3ff848a58cf17929eb753bfcaf16e
Arash Shabbeh <arashaan@live.com>
2020-11-09 20:23:30 +0330

    Add json handler extensiion to get json property name.

---
+++ src/netcoreapp3.1/R8.Lib/JsonExtensions/JsonHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace R8.Lib.JsonExtensions
+{
+    /// <summary>
+    /// An static class to get some information about specified type.
+    /// </summary>
+    /// <typeparam name="T">A generic type of class.</typeparam>
+    public static class JsonHandler<T> where T : class
+    {
+        /// <summary>
+        /// Represents name of property.
+        /// </summary>
+        /// <param name="expression">An <see cref="Expression{TDelegate}"/> value that representing specific property.</param>
+        /// <returns>An <see cref="string"/> value.</returns>
+        /// <remarks>If <c>JsonPropertyAttribute</c> is null returns Property's name, otherwise it will return <c>JsonPropertyAttribute</c> value.</remarks>
+        public static string GetProperty(Expression<Func<T, object>> expression)
+        {
+            var property = expression.GetPropertyInfo();
+            return property.GetJsonName();
+        }
+    }
+}
