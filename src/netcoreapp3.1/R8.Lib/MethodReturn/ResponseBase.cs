be88df9074bf85de0a045261b65aff3ca819b718
Arash Shabbeh <arashaan@live.com>
2020-11-10 16:16:36 +0330

    Remove redundant property

--- src/netcoreapp3.1/R8.Lib/MethodReturn/ResponseBase.cs
+++ src/netcoreapp3.1/R8.Lib/MethodReturn/ResponseBase.cs
@@ -3,11 +3,10 @@ using R8.Lib.Localization;
 
 namespace R8.Lib.MethodReturn
 {
-    public abstract class ResponseBase : IResponseBase
+    public class ResponseBase : IResponseBase
     {
         public Flags Status { get; set; }
         public string Message => this.GetMessage();
-        public ValidatableResultCollection Errors { get; set; }
         public ILocalizer Localizer { get; set; }
 
         public void SetLocalizer(ILocalizer localizer)
