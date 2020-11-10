5922de510fecdf6b80a37c070dee0e660e0cef60
Arash Shabbeh <arashaan@live.com>
2020-11-10 16:17:28 +0330

    Move AddLocalizer from R8.AspNetCore to R8.Lib

---
+++ src/aspcore3.1/R8.AspNetCore/Localization/LocalizerDIConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+using R8.Lib.Localization;
+
+using System;
+
+namespace R8.AspNetCore.Localization
+{
+    public static class LocalizerDIConfiguration
+    {
+        public static IApplicationBuilder UseLocalizer(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                var serviceProvider = context.RequestServices;
+                var localizer = serviceProvider.GetService<ILocalizer>();
+                if (localizer == null)
+                    throw new NullReferenceException($"{nameof(ILocalizer)} must be registered as a service.");
+
+                await localizer.RefreshAsync();
+                await next();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/netcoreapp3.1/R8.Lib/Localization/LocalizerConfigure.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using R8.Lib.MethodReturn;
+
+using System;
+
+namespace R8.Lib.Localization
+{
+    public static class LocalizerConfigure
+    {
+        /// <summary>
+        /// Registers <see cref="ILocalizer"/> as a service.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddLocalizer(this IServiceCollection services, Action<LocalizerConfiguration> config)
+        {
+            services.AddSingleton<ILocalizer>(serviceProvider =>
+            {
+                using var scope = serviceProvider.CreateScope();
+
+                var configuration = new LocalizerConfiguration();
+                config(configuration);
+
+                return new Localizer(configuration);
+            });
+
+            services.AddTransient<IResponseBase>(serviceProvider =>
+            {
+                using var scope = serviceProvider.CreateScope();
+
+                var localizer = scope.ServiceProvider.GetService<ILocalizer>();
+                if (localizer == null)
+                    throw new NullReferenceException($"Can't find {nameof(ILocalizer)} related service");
+
+                var obj = new ResponseBase();
+                obj.SetLocalizer(localizer);
+                return obj;
+            });
+            return services;
+        }
+    }
+}
