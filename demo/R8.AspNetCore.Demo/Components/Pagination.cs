9892aab0a5c9bf12e0ea278989b7991f9a287281
Arash Shabbeh <arashaan@live.com>
2020-11-23 17:38:48 +0330

    Update libraries

---
+++ demo/R8.AspNetCore.Demo/Components/Pagination.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using R8.AspNetCore.Routing;
+
+namespace R8.AspNetCore.Demo.Components
+{
+    public class Pagination : ViewComponent
+    {
+        public IViewComponentResult Invoke() => this.InvokePagination("~/", "List");
+    }
+}
