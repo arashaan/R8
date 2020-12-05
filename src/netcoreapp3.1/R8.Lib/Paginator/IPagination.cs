2db3b9cbeb33319df6eb7bb84db67e20ae2ea3c0
Arash Shabbeh <arashaan@live.com>
2020-12-05 17:03:16 +0330

    Remove setter method from interface

--- src/netcoreapp3.1/R8.Lib/Paginator/IPagination.cs
+++ src/netcoreapp3.1/R8.Lib/Paginator/IPagination.cs
@@ -1,20 +1,23 @@
 ﻿namespace R8.Lib.Paginator
 {
+    /// <summary>
+    /// A base interface for <see cref="Pagination"/>.
+    /// </summary>
     public interface IPagination
     {
         /// <summary>
         /// An <see cref="int"/> value that representing page number that contains these data
         /// </summary>
-        public int CurrentPage { get; set; }
+        public int CurrentPage { get; }
 
         /// <summary>
         /// An <see cref="int"/> value that representing page numbers
         /// </summary>
-        public int Pages { get; set; }
+        public int Pages { get; }
 
         /// <summary>
         /// An <see cref="int"/> value that representing loaded data count
         /// </summary>
-        public int CountAll { get; set; }
+        public int CountAll { get; }
     }
 }
