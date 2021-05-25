4563ce5317b3fcb98cf4f14001899aaf353a2dcd
Arash ( I am R8 ) <arash.shabbeh@gmail.com>
2021-05-25 15:33:03 +0430

    Added documentations

--- src/R8.Lib/EnumReflections.cs
+++ src/R8.Lib/EnumReflections.cs
@@ -9,10 +9,19 @@ namespace R8.Lib
 {
     public static class EnumReflections
     {
+        /// <summary>
+        /// Returns specific <see cref="Attribute"/> from given enum member.
+        /// </summary>
+        /// <typeparam name="TAttribute"></typeparam>
+        /// <param name="enumMember"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
         public static TAttribute GetAttribute<TAttribute>(this Enum enumMember)
           where TAttribute : Attribute
         {
-            if (enumMember == null) throw new ArgumentNullException(nameof(enumMember));
+            if (enumMember == null)
+                throw new ArgumentNullException(nameof(enumMember));
+
             var enumType = enumMember.GetType();
             var enumName = Enum.GetName(enumType, enumMember);
             var memberInfos = enumType.GetMember(enumName);
