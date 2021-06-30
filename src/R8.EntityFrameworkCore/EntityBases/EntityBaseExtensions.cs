2efd32589b3e018a35d560d92b1461ef6705076b
Arash ( I am R8 ) <arash.shabbeh@gmail.com>
2021-07-01 00:18:46 +0300

    Move audit configurations

--- src/R8.EntityFrameworkCore/EntityBases/EntityBaseExtensions.cs
+++ src/R8.EntityFrameworkCore/EntityBases/EntityBaseExtensions.cs
@@ -1,6 +1,6 @@
-﻿using Humanizer;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 using R8.EntityFrameworkCore.Audits;
 
 namespace R8.EntityFrameworkCore.EntityBases
@@ -12,20 +12,6 @@ namespace R8.EntityFrameworkCore.EntityBases
             builder.ToTable(Extensions.NormalizeTableName(typeof(TEntity)));
             builder.HasKey(x => x.Id);
             builder.Property(e => e.Id).ValueGeneratedOnAdd();
-            return builder;
-        }
-
-        internal static EntityTypeBuilder<TEntity> ApplyAuditConfiguration<TEntity>(this EntityTypeBuilder<TEntity> builder) where TEntity : class, IEntityBaseAudit
-        {
-            builder.OwnsMany(x => x.Audits, action =>
-            {
-                action.WithOwner().HasForeignKey(x => x.RowId);
-                action.ToTable($"{builder.GetTableName()}_{nameof(Audit).Pluralize()}");
-                action.HasKey(x => x.Id);
-                action.Property(x => x.Id).ValueGeneratedOnAdd();
-                action.Property(x => x.Additional).HasJsonConversion();
-                action.Property(x => x.Changes).HasJsonConversion();
-            });
             return builder;
         }
 
