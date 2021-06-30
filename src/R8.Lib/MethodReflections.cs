1e52b65b7bc7ea79019d01470078d80b44543b89
Arash ( I am R8 ) <arash.shabbeh@gmail.com>
2021-07-01 00:19:43 +0300

    Added check for method's overrides

---
+++ src/R8.Lib/MethodReflections.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace R8.Lib
+{
+    public static class MethodReflections
+    {
+        /// <summary>
+        /// Checks if given method is overriden from base type.
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns>A <see cref="bool"/>.</returns>
+        public static bool IsOverriden(this MethodInfo method)
+        {
+            return method.GetBaseDefinition().DeclaringType != method.DeclaringType;
+        }
+    }
+}
